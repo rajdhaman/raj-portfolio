0c8b4821b6f3da2af21c48a0686e5008e17b4b5e
gujjarsaab01 <113016797+gujjarsaab01@users.noreply.github.com>
2023-05-06 18:17:22 +0530

    code update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,8 +3,8 @@ import LogoTitle from '../../assets/images/logo-s3.png'
 import './index.scss'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
-import Logo from './Logo'
 import Loader from 'react-loaders'
+import Logo from './Logo'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
